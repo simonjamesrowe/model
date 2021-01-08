b6a8ad3f8e1d1dffcb9da2bce6810bc775a9f60f
Simon Rowe <simon.rowe@gmail.com>
2021-01-08 22:08:40 +0000

    change scope of required dependencies to api instead of implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-json")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	api("org.springframework.boot:spring-boot-starter-json")
+	api("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
