2cf6c49e7ff148cbb387ff1da014fea55f58a8a5
Simon Rowe <simon.rowe@gmail.com>
2021-02-10 07:35:45 +0000

    add contants for webhook event type.

--- src/main/kotlin/com/simonjamesrowe/model/data/Constants.kt
+++ src/main/kotlin/com/simonjamesrowe/model/data/Constants.kt
@@ -2,4 +2,7 @@ package com.simonjamesrowe.model.data
 
 object Constants {
   const val TYPE_BLOG = "blog"
+  const val TYPE_JOB = "job"
+  const val TYPE_SKILL = "skills"
+  const val TYPE_SKILLS_GROUP = "skills-group"
 }
